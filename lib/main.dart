fdb3cd0dae521c77a70f2398fa3c920ded23098f
rsyd29 <brasyid15@gmail.com>
2021-10-19 15:55:25 +0700

    Create and making customizing the MaterialApp with new theme primary Colors.grey and secondary Colors.black

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,27 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
+  /// 1. A widget’s build() method is the entry point for composing together other widgets to make a new widget.
   @override
   Widget build(BuildContext context) {
+    /// 2. A theme determines visual aspects like color. The default ThemeData will show the standard Material defaults.
+    final ThemeData theme = ThemeData();
+
+    /// 3. MaterialApp uses Material Design and is the widget that will be included in RecipeApp.
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      /// 4. The title of the app is a description that the device uses to identify the app. The UI won’t display this.
+      title: 'Recipe Calculator',
+
+      /// 5. By copying the theme and replacing the color scheme with an updated copy lets you change the app’s colors. Here, the primary color is Colors.grey and the secondary color is Colors.black.
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.grey,
+          secondary: Colors.black,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      /// 6. This still uses the same MyHomePage widget as before, but now, you’ve updated the title and displayed it on the device.
+      home: const MyHomePage(title: 'Recipe Calculator'),
     );
   }
 }
