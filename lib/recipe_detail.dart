01523d8626b7530ee227bcda613129a468f60c21
rsyd29 <brasyid15@gmail.com>
2021-10-19 17:26:27 +0700

    Showing the ingredients in recipe detail screen

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -43,6 +43,21 @@ class _RecipeDetailState extends State<RecipeDetail> {
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
             ),
+            // 7. An Expanded widget, which expands to fill the space in a Column. This way, the ingredient list will take up the space not filled by the other widgets.
+            Expanded(
+              // 8. A ListView, with one row per ingredient.
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (context, index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  // 9. A Text that uses string interpolation to populate a string with runtime values. You use the ${expression} syntax inside the string literal to denote these.
+                  // TODO: Add ingredient.quantity
+                  return Text(
+                      '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}');
+                },
+              ),
+            )
           ],
         ),
       ),
